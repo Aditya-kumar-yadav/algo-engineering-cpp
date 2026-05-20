199b604fb6e5b498b08a3b99748cb9f10d04ea0e
Aditya Kumar Yadav <yraj15927@gmail.com>
2026-05-20 13:23:52 +0530

    Added Two Sum soution

--- src/ExampleTest/solution.cpp
+++ src/ExampleTest/solution.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
+#include <vector>
+#include <unordered_map>
+
 using namespace std;
 
 int main() {
-    int a, b;
-    // Reads two integers from input
-    if (cin >> a >> b) {
-        // Outputs their sum
-        cout << a + b << endl;
+    int n, target;
+    if (!(cin >> n)) return 0;
+    
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+    cin >> target;
+
+    unordered_map<int, int> numMap;
+    for (int i = 0; i < n; i++) {
+        int complement = target - nums[i];
+        if (numMap.find(complement) != numMap.end()) {
+            cout << numMap[complement] << " " << i << endl;
+            return 0;
+        }
+        numMap[nums[i]] = i;
     }
     return 0;
 }
