9870184e31111a1b6e24b2ceb56056608c05908f
Aditya Kumar Yadav <yraj15927@gmail.com>
2026-05-20 13:07:09 +0530

    added First solution

---
+++ src/ExampleTest/solution.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    // Reads two integers from input
+    if (cin >> a >> b) {
+        // Outputs their sum
+        cout << a + b << endl;
+    }
+    return 0;
+}
